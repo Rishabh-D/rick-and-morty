4123ca7731db6f9c3442a95f83885d79a20c3b75
Rishabh-D <dubeyrishabh.rd@gmail.com>
2022-10-10 22:31:43 +0530

    fetching without react query

---
+++ src/components/Characters.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+const Characters = () => {
+  const [characters, setCharacters] = useState([])
+
+  const fetchCharacters = async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const data = await response.json()
+    setCharacters(data.results)
+  }
+
+  useEffect(()=>{
+    fetchCharacters()
+  },[])
+
+  return (
+    <div>
+      {characters.map(character => <div>{character.name}</div>)}
+    </div>
+  );
+};
+
+export default Characters;
