1a8d65ccfd6e5a6c5e2333bd442a4cdbe58bdfcf
Dubey Rishabh MT-IMSG <Rishabh.Dubey@mt.com>
2022-10-11 08:59:43 +0530

    added pagination, isLoading and isError functionality

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from "react-query"
 import Character from './Character';
+
 const Characters = () => {
+
+  const [page, setPage] = useState(1)
   const fetchCharacters = async () => {
-    const response = await fetch('https://rickandmortyapi.com/api/character')
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
     return await response.json()
   }
 
-  const {data, status} = useQuery("characters", fetchCharacters)
+  const {data, status, isPreviousData, isLoading, isError} = useQuery(["characters",page], fetchCharacters, {keepPreviousData:true})
   
-  if (status === "loading"){
+  if (isLoading){
     return <div>Loading...</div>
   }
-  else if(status === "error"){
+  else if(isError){
     return <div>Error</div>
   }
 
-
+  console.log(page)
   
   
   return (
@@ -24,6 +27,10 @@ const Characters = () => {
       <h1>Rick and Morty</h1>
       <div className='characters'>
         {data.results.map(character => <Character key={character.id} character={character}></Character>)}
+      </div>
+      <div className='Pagination-btn'>
+        <button disabled = {page===1} onClick={() => setPage(page=> page-1)}>Prev</button>
+        <button disabled = {data.info.next === null && isPreviousData} onClick={() => setPage(page=> page+1)}>Next</button>
       </div>
     </div>
   );
